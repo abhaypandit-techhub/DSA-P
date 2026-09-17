35e35ede1eb4cf24c0ab2918e6b581b30d7b31bb
Abhay pandit <abhaytech12cy@gmail.com>
2026-09-18 00:05:32 +0530

    Delete BubbleSort.py

--- BubbleSort.py
+++
@@ -1,7 +0,0 @@
-numbs=[3,5,6,4,8,9,10,7,1]
-n=len(numbs)
-for i in range(0,n):
-  for j in range(0,n-i-1):
-    if numbs[j]>numbs[j+1]:
-      numbs[j],numbs[j+1]=numbs[j+1],numbs[j]
-print(numbs)      
